100f08a6eca6b32d0478eba8c62c4ca92781f54d
LinkWu <wjianl@qq.com>
2022-03-23 13:37:01 +0800

    优化

--- main.sh
+++ main.sh
@@ -403,7 +403,7 @@ if (( 0 != $step_upload )); then
     suffix=${u3d_out_file_name##*.}
     if [[ "exe" == $suffix ]]; then
         if type "zip" &> /dev/null; then
-            zip -rj $name $u3d_out_path
+            zip -Dr $name $u3d_out_path
         elif type "7z" &> /dev/null; then
             7z a -tzip $name $u3d_out_path/*
         else
