069b4a4d72d4f8493b59ac1b493d4b80d361cc22
LinkWu <wjianl@qq.com>
2022-03-24 16:04:16 +0800

    优化

--- dot_prj_stage1/src/Lib.cs
+++ dot_prj_stage1/src/Lib.cs
@@ -27,83 +27,91 @@ namespace com.wolfired.dot_prj_stage1
 
     public class UnityEditorHelper
     {
+        internal static void SetupGradle()
+        {
+            var prefs_key_GradleUseEmbedded = "GradleUseEmbedded";
+            // var prefs_key_GradlePath = "GradlePath";
+            // var prefs_key_AndroidGradleStopDaemonsOnExit = "AndroidGradleStopDaemonsOnExit";
+            EditorPrefs.SetInt(prefs_key_GradleUseEmbedded, 1);
+        }
+
         internal static void SetupJDKAndroidSDKNDK()
         {
-            var prefs_key_jdk_root = "JdkPath";
-            var prefs_val_jdk_root = "";
+            var prefs_key_JdkPath = "JdkPath";
+            var prefs_val_JdkPath = "";
 
-            if (EditorPrefs.HasKey(prefs_key_jdk_root))
+            if (EditorPrefs.HasKey(prefs_key_JdkPath))
             {
-                prefs_val_jdk_root = EditorPrefs.GetString(prefs_key_jdk_root);
+                prefs_val_JdkPath = EditorPrefs.GetString(prefs_key_JdkPath);
             }
 
-            if (null == prefs_val_jdk_root || "" == prefs_val_jdk_root || !Directory.Exists(prefs_val_jdk_root))
+            if (null == prefs_val_JdkPath || "" == prefs_val_JdkPath || !Directory.Exists(prefs_val_JdkPath))
             {
-                var env_key_java_home = "JAVA_HOME";
-                prefs_val_jdk_root = Environment.GetEnvironmentVariable(env_key_java_home);
-                if (null == prefs_val_jdk_root || "" == prefs_val_jdk_root || !Directory.Exists(prefs_val_jdk_root))
+                var env_key_JAVA_HOME = "JAVA_HOME";
+                prefs_val_JdkPath = Environment.GetEnvironmentVariable(env_key_JAVA_HOME);
+                if (null == prefs_val_JdkPath || "" == prefs_val_JdkPath || !Directory.Exists(prefs_val_JdkPath))
                 {
-                    Debug.Log("You Need Setup Env Var: " + env_key_java_home);
+                    Debug.Log("You Need Setup Env Var: " + env_key_JAVA_HOME);
                 }
                 else
                 {
-                    EditorPrefs.SetString(prefs_key_jdk_root, prefs_val_jdk_root);
+                    EditorPrefs.SetString(prefs_key_JdkPath, prefs_val_JdkPath);
                 }
             }
-            Debug.Log(prefs_key_jdk_root + " = " + prefs_val_jdk_root);
+            Debug.Log(prefs_key_JdkPath + " = " + prefs_val_JdkPath);
 
-            var prefs_key_ndk_root = "AndroidNdkRoot";
-            var prefs_val_ndk_root = "";
-            if (EditorPrefs.HasKey(prefs_key_ndk_root))
+            var prefs_key_AndroidNdkRoot = "AndroidNdkRoot";
+            var prefs_val_AndroidNdkRoot = "";
+            if (EditorPrefs.HasKey(prefs_key_AndroidNdkRoot))
             {
-                prefs_val_ndk_root = EditorPrefs.GetString(prefs_key_ndk_root);
+                prefs_val_AndroidNdkRoot = EditorPrefs.GetString(prefs_key_AndroidNdkRoot);
             }
 
-            if (null == prefs_val_ndk_root || "" == prefs_val_ndk_root || !Directory.Exists(prefs_val_ndk_root))
+            if (null == prefs_val_AndroidNdkRoot || "" == prefs_val_AndroidNdkRoot || !Directory.Exists(prefs_val_AndroidNdkRoot))
             {
-                var env_key_ndk_root = "ANDROID_NDK_ROOT";
-                prefs_val_ndk_root = Environment.GetEnvironmentVariable(env_key_ndk_root);
-                if (null == prefs_val_ndk_root || "" == prefs_val_ndk_root || !Directory.Exists(prefs_val_ndk_root))
+                var env_key_ANDROID_NDK_ROOT = "ANDROID_NDK_ROOT";
+                prefs_val_AndroidNdkRoot = Environment.GetEnvironmentVariable(env_key_ANDROID_NDK_ROOT);
+                if (null == prefs_val_AndroidNdkRoot || "" == prefs_val_AndroidNdkRoot || !Directory.Exists(prefs_val_AndroidNdkRoot))
                 {
-                    var env_key_ndk_home = "ANDROID_NDK_HOME";
-                    prefs_val_ndk_root = Environment.GetEnvironmentVariable(env_key_ndk_home);
-                    if (null == prefs_val_ndk_root || "" == prefs_val_ndk_root || !Directory.Exists(prefs_val_ndk_root))
+                    var env_key_ANDROID_NDK_HOME = "ANDROID_NDK_HOME";
+                    prefs_val_AndroidNdkRoot = Environment.GetEnvironmentVariable(env_key_ANDROID_NDK_HOME);
+                    if (null == prefs_val_AndroidNdkRoot || "" == prefs_val_AndroidNdkRoot || !Directory.Exists(prefs_val_AndroidNdkRoot))
                     {
-                        Debug.Log("You Need Setup Env Var: " + env_key_ndk_root);
+                        Debug.Log("You Need Setup Env Var: " + env_key_ANDROID_NDK_ROOT);
                     }
                     else
                     {
-                        EditorPrefs.SetString(prefs_key_ndk_root, prefs_val_ndk_root);
+                        EditorPrefs.SetString(prefs_key_AndroidNdkRoot, prefs_val_AndroidNdkRoot);
                     }
                 }
                 else
                 {
-                    EditorPrefs.SetString(prefs_key_ndk_root, prefs_val_ndk_root);
+                    EditorPrefs.SetString(prefs_key_AndroidNdkRoot, prefs_val_AndroidNdkRoot);
                 }
             }
-            Debug.Log(prefs_key_ndk_root + " = " + prefs_val_ndk_root);
+            Debug.Log(prefs_key_AndroidNdkRoot + " = " + prefs_val_AndroidNdkRoot);
 
-            var prefs_key_sdk_root = "AndroidSdkRoot";
-            var prefs_val_sdk_root = "";
-            if (EditorPrefs.HasKey(prefs_key_sdk_root))
+            var prefs_key_AndroidSdkRoot = "AndroidSdkRoot";
+            var prefs_val_AndroidSdkRoot = "";
+            if (EditorPrefs.HasKey(prefs_key_AndroidSdkRoot))
             {
-                prefs_val_sdk_root = EditorPrefs.GetString(prefs_key_sdk_root);
+                prefs_val_AndroidSdkRoot = EditorPrefs.GetString(prefs_key_AndroidSdkRoot);
             }
 
-            if (null == prefs_val_sdk_root || "" == prefs_val_sdk_root || !Directory.Exists(prefs_val_sdk_root))
+            if (null == prefs_val_AndroidSdkRoot || "" == prefs_val_AndroidSdkRoot || !Directory.Exists(prefs_val_AndroidSdkRoot))
             {
-                var env_key_sdk_root = "ANDROID_SDK_ROOT";
-                prefs_val_sdk_root = Environment.GetEnvironmentVariable(env_key_sdk_root);
-                if (null == prefs_val_sdk_root || "" == prefs_val_sdk_root || !Directory.Exists(prefs_val_sdk_root))
+                var env_key_ANDROID_SDK_ROOT = "ANDROID_SDK_ROOT";
+                prefs_val_AndroidSdkRoot = Environment.GetEnvironmentVariable(env_key_ANDROID_SDK_ROOT);
+                if (null == prefs_val_AndroidSdkRoot || "" == prefs_val_AndroidSdkRoot || !Directory.Exists(prefs_val_AndroidSdkRoot))
                 {
-                    Debug.Log("You Need Setup Env Var: " + env_key_sdk_root);
+                    Debug.Log("You Need Setup Env Var: " + env_key_ANDROID_SDK_ROOT);
                 }
                 else
                 {
-                    EditorPrefs.SetString(prefs_key_sdk_root, prefs_val_sdk_root);
+                    EditorPrefs.SetString(prefs_key_AndroidSdkRoot, prefs_val_AndroidSdkRoot);
                 }
             }
-            Debug.Log(prefs_key_sdk_root + " = " + prefs_val_sdk_root);
+            Debug.Log(prefs_key_AndroidSdkRoot + " = " + prefs_val_AndroidSdkRoot);
         }
 
         public static void SetupVSCode()
