c9367e5c339b957a19ffb2eb3b488a0f6380f59c
LinkWu <wjianl@qq.com>
2022-04-02 17:39:27 +0800

    Test

---
+++ u3d_prj/Assets/Default.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Default : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+         foreach (var assembly in assemblies)
+         {
+             var types = assembly.GetTypes();
+             foreach (var type in types)
+             {
+                 if("com.wolfired.dot_prj_core.Booter" == type.FullName)
+                 {
+                     this.gameObject.AddComponent(type);
+                 }
+             }
+         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
