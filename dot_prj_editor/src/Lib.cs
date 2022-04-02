79f313408a642f91479af611937fa7f89a60695e
LinkWu <wjianl@qq.com>
2022-04-02 13:59:49 +0800

    Test

--- dot_prj_editor/src/Lib.cs
+++ dot_prj_editor/src/Lib.cs
@@ -1,5 +1,53 @@
 using System;
+using UnityEditor;
+using UnityEngine;
+using com.wolfired.dot_prj_core;
 
-namespace com.wolfired.dot_prj_editor {
+namespace com.wolfired.dot_prj_editor
+{
     public class Lib { }
+
+    [InitializeOnLoad]
+    public class InitializeOnX
+    {
+        static InitializeOnX()
+        {
+            AssemblyReloadEvents.beforeAssemblyReload += () =>
+            {
+                Debug.Log("AssemblyReloadEvents.beforeAssemblyReload");
+            };
+
+            Debug.Log("InitializeOnLoad");
+
+            AssemblyReloadEvents.afterAssemblyReload += () =>
+            {
+                Debug.Log("AssemblyReloadEvents.afterAssemblyReload");
+
+                var go = GameObject.Find("Main Camera");
+                if (null != go)
+                {
+                    Booter booter = null;
+                    if (!go.TryGetComponent<Booter>(out booter))
+                    {
+                        go.AddComponent<Booter>();
+
+                        AssetDatabase.SaveAssets();
+                        AssetDatabase.Refresh();
+                    }
+                }
+            };
+        }
+
+        // [InitializeOnLoadMethod]
+        // public static void InitializeOnLoadMethod()
+        // {
+        //     Debug.Log("InitializeOnLoadMethod");
+        // }
+
+        // [InitializeOnEnterPlayMode]
+        // public static void InitializeOnEnterPlayMode(EnterPlayModeOptions options)
+        // {
+        //     Debug.Log("InitializeOnEnterPlayMode: " + options);
+        // }
+    }
 }
